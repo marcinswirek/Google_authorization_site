22446a35b1acb91fa7b121c90bb58ab861446302
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-03 19:40:55 +0200

    Copy files from previous task. Install npm packages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# zadanie-17-7-google-auth
+# zadanie-17-6-html-templates
+
+```
+'npm install' to install necessary npm packages
+'npm start' to start and app watching using Nodemon
+-enter link: http://localhost:3000/
+```
---
+++ js/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '385088869234-7ki9mndlert7e1mmp301g0p1d390asmj.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'CSE_ZL376_4mbP-U1ANFsxrc',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
